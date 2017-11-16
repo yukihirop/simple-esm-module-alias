94bc7fc1f6ab2339346c63dcda65a73c1660e959
ChunQhai <abelchun@ChunQhais-MBP.base.withvase.com>
2017-11-16 13:59:32 +0800

    First commit with issue

---
+++ app.mjs
@@ -0,0 +1,15 @@
+require('module-alias/register');
+import http from 'http';
+import express from 'express';
+
+import rand from '@lib/random'; // Default './lib/random'
+
+console.log("RAND: " + rand.sayHello);
+
+let app = express();
+app.server = http.createServer(app);
+app.server.listen(8088, () => {
+	console.log(`Started on port ${app.server.address().port}`);
+});
+
+export default app;
