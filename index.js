31d786df1c9b2e7393697682641ca86664f37574
yukihirop <te108186@gmail.com>
2021-02-20 03:23:03 +0900

    Add pattern when using require('module-alias').addAliases()

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 loader = require('@std/esm')(module)
-loader('module-alias/register');
+
+// case.1 When writing moduleAlias ​​in package.json
+// loader('module-alias/register');
+
+// case.2 When registering using addAliases
+const pkgDir = require('pkg-dir');
+const projectRoot = pkgDir.sync() || process.cwd();
+loader('module-alias').addAliases({
+  "@root": projectRoot + "/src",
+  "@lib": projectRoot + "/lib"
+})
 
 module.exports = loader('./app.mjs')
